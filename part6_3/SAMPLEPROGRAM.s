96ab48ce5f6375986fdaa0b0fdd032d837d243e6
Shihara Dewagedara <eranjan1020@gmail.com>
2025-07-21 12:36:21 +0530

    add new files

--- part6_3/SAMPLEPROGRAM.s
+++ part6_3/SAMPLEPROGRAM.s
@@ -1,11 +1,5 @@
-loadi 0 0x09    
-loadi 1 0x01    
-swd 0 1       
-swi 1 0x00    
-lwd 2 1       
-lwd 3 1       
-sub 4 0 1     
-swi 4 0x02    
-lwi 5 0x02     
-swi 4 0x20     
-lwi 6 0x20
+loadi 4 0x25
+loadi 5 0x35
+add 6 4 5
+loadi 1 0x90
+sub 1 1 4
